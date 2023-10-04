627f3ae338dcc7b6262f8655d0c6ae5e3739862b
Pooya Golchian <pooya.golchian@gmail.com>
2023-10-04 17:09:41 +0400

    update code and files

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-const fs = require("fs");
-const XLSX = require("xlsx");
-const jsonData = fs.readFileSync("data.json", "utf-8");
-const data = JSON.parse(jsonData);
-const keyValuePairs = [];
-for (const key in data) {
-  if (data.hasOwnProperty(key)) {
-    const value = data[key];
-    const keyValueString = `${key}: ${value}`;
-    keyValuePairs.push(keyValueString);
+const fs = require('fs');
+const XLSX = require('xlsx');
+
+// Function to convert JSON to XLSX with separate rows for each key-value pair
+function convertJSONtoXLSX(jsonData) {
+  const wsData = [];
+  for (const item of jsonData) {
+    for (const key of Object.keys(item)) {
+      const row = {
+        String: key,
+        Value: item[key],
+      };
+      wsData.push(row);
+    }
   }
+
+  const ws = XLSX.utils.json_to_sheet(wsData);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Sheet 1');
+  const xlsxBuffer = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+  return xlsxBuffer;
 }
-const wb = XLSX.utils.book_new();
-const ws = XLSX.utils.aoa_to_sheet(keyValuePairs.map((pair) => [pair]));
-XLSX.utils.book_append_sheet(wb, ws, "Sheet 1");
-XLSX.writeFile(wb, "output.xlsx");
+
+// Read JSON files and convert to XLSX
+function processJSONFiles() {
+  fs.readdirSync('./jsonFiles').forEach((file) => {
+    if (file.endsWith('.json')) {
+      const jsonData = JSON.parse(
+        fs.readFileSync(`./jsonFiles/${file}`, 'utf-8')
+      );
+      const xlsxBuffer = convertJSONtoXLSX(jsonData);
+      fs.writeFileSync(
+        `./xlsxFiles/${file.replace('.json', '.xlsx')}`,
+        xlsxBuffer
+      );
+      console.log(`Converted ${file} to XLSX.`);
+    }
+  });
+}
+
+// Create output directory if it doesn't exist
+if (!fs.existsSync('./xlsxFiles')) {
+  fs.mkdirSync('./xlsxFiles');
+}
+
+// Start the conversion process
+processJSONFiles();
